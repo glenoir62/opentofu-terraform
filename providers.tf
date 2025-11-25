e2497f9265f26934256d3bd7fc9caa112fe61c9f
Guillaume Lenoir <guillaume.lenoir@ext.adeo.com>
2025-11-25 16:40:59 +0100

    Update provider source to OpenTofu registry and adjust version constraints

--- providers.tf
+++ providers.tf
@@ -1,8 +1,10 @@
 terraform {
+  required_version = ">= 1.6.0"
+
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 3.0.1"
+      version = "~> 3.0"
     }
   }
 }
