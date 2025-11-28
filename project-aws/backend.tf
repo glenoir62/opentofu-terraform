f44a0cfe8ca2f70c97bb446deab498c31c60f7fc
Guillaume Lenoir <guillaume.lenoir@ext.adeo.com>
2025-11-28 15:18:21 +0100

    Disable Terraform state locking by commenting out `use_lockfile` in backend configuration.

--- project-aws/backend.tf
+++ project-aws/backend.tf
@@ -4,7 +4,7 @@ terraform {
     bucket = "gleo-tfstate-bucket-projet1-unique-12345"
     key    = "project-aws/terraform.tfstate" # Chemin du fichier d'état dans le bucket pour ce projet
     region = "eu-west-3"                          # Région de votre backend
-    use_lockfile = true
+    //use_lockfile = true
     workspace_key_prefix = "env-"
     encrypt      = true
 
